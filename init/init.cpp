9c7b356b7c852f28d1b41a0852f04f239d0ef6b6
Aidas <aidas957@users.noreply.github.com>
2016-03-19 12:04:45 +0200

    Update init.cpp

--- init/init.cpp
+++ init/init.cpp
@@ -1020,6 +1020,24 @@ static int charging_mode_booting(void) {
 #endif
 }
 
+static int charging_mode_booting(void) {
+#ifndef BOARD_CHARGING_MODE_BOOTING_LPM
+    return 0;
+#else
+    int f;
+    char cmb;
+    f = open(BOARD_CHARGING_MODE_BOOTING_LPM, O_RDONLY);
+    if (f < 0)
+        return 0;
+
+    if (1 != read(f, (void *)&cmb,1))
+        return 0;
+
+    close(f);
+    return ('1' == cmb);
+#endif
+}
+
 int main(int argc, char** argv) {
     if (!strcmp(basename(argv[0]), "ueventd")) {
         return ueventd_main(argc, argv);
